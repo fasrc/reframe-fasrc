5323db65d44c6b37b4e9afa19f07fc2422ee6fce
Paul Edmon <pedmon@cfa.harvard.edu>
2021-02-03 09:46:50 -0500

    Updating test for FASRC environment.

--- microbenchmarks/cpu/alloc_speed/alloc_speed.py
+++ microbenchmarks/cpu/alloc_speed/alloc_speed.py
@@ -1,4 +1,5 @@
 # Copyright 2016-2021 Swiss National Supercomputing Centre (CSCS/ETH Zurich)
+# Copyright 2021 FAS Research Computing Harvard University
 # ReFrame Project Developers. See the top-level LICENSE file for details.
 #
 # SPDX-License-Identifier: BSD-3-Clause
@@ -14,53 +15,11 @@ class AllocSpeedTest(rfm.RegressionTest):
         self.sourcepath = 'alloc_speed.cpp'
         self.build_system = 'SingleSource'
         self.build_system.cxxflags = ['-O3', '-std=c++11']
-        self.valid_systems = ['daint:gpu', 'daint:mc', 'dom:gpu', 'dom:mc']
-        self.valid_prog_environs = ['PrgEnv-gnu']
-        if hugepages == 'no':
-            self.valid_systems += ['arolla:cn', 'arolla:pn',
-                                   'tsa:cn', 'tsa:pn']
-        else:
-            if self.current_system.name in {'dom', 'daint'}:
-                self.modules = ['craype-hugepages%s' % hugepages]
+        self.valid_systems = ['*']
+        self.valid_prog_environs = ['*']
 
         self.sanity_patterns = sn.assert_found('4096 MB', self.stdout)
         self.perf_patterns = {
             'time': sn.extractsingle(r'4096 MB, allocation time (?P<time>\S+)',
                                      self.stdout, 'time', float)
         }
-        self.sys_reference = {
-            'no': {
-                'dom:gpu': {
-                    'time': (1.22, None, 0.05, 's')
-                },
-                'dom:mc': {
-                    'time': (1.41, None, 0.05, 's')
-                },
-                'daint:gpu': {
-                    'time': (1.22, None, 0.05, 's')
-                },
-                'daint:mc': {
-                    'time': (1.41, None, 0.05, 's')
-                },
-            },
-            '2M': {
-                'dom:gpu': {
-                    'time': (0.11, None, 0.10, 's')
-                },
-                'dom:mc': {
-                    'time': (0.20, None, 0.10, 's')
-                },
-                'daint:gpu': {
-                    'time': (0.11, None, 0.10, 's')
-                },
-                'daint:mc': {
-                    'time': (0.20, None, 0.10, 's')
-                },
-                '*': {
-                    'time': (0, None, None, 's')
-                }
-            },
-        }
-        self.reference = self.sys_reference[hugepages]
-        self.maintainers = ['AK', 'VH']
-        self.tags = {'production', 'craype'}
