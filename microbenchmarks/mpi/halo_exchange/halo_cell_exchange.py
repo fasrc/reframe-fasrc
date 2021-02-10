3818e8ce2bb09d22ab7e3b6ad16e169a69e78651
Paul Edmon <pedmon@cfa.harvard.edu>
2021-02-10 15:46:49 -0500

    halo_exchange is working.

--- microbenchmarks/mpi/halo_exchange/halo_cell_exchange.py
+++ microbenchmarks/mpi/halo_exchange/halo_cell_exchange.py
@@ -1,4 +1,5 @@
 # Copyright 2016-2021 Swiss National Supercomputing Centre (CSCS/ETH Zurich)
+# Copyright 2021 FAS Research Computing Harvard University
 # ReFrame Project Developers. See the top-level LICENSE file for details.
 #
 # SPDX-License-Identifier: BSD-3-Clause
@@ -14,10 +15,8 @@ class HaloCellExchangeTest(rfm.RegressionTest):
         self.sourcepath = 'halo_cell_exchange.c'
         self.build_system = 'SingleSource'
         self.build_system.cflags = ['-O2']
-        self.valid_systems = ['daint:gpu', 'dom:gpu',
-                              'arolla:cn', 'tsa:cn']
-        self.valid_prog_environs = ['PrgEnv-cray', 'PrgEnv-pgi',
-                                    'PrgEnv-gnu']
+        self.valid_systems = ['test:rc-testing']
+        self.valid_prog_environs = ['gnu-mpi','intel-mpi']
         self.num_tasks = 6
         self.num_tasks_per_node = 1
         self.num_gpus_per_node = 0
@@ -66,38 +65,7 @@ class HaloCellExchangeTest(rfm.RegressionTest):
                 'time_mpi', float)
         }
 
-        self.reference = {
-            'dom:gpu': {
-                'time_2_10': (3.925395e-06, None, 0.50, 's'),
-                'time_2_10000': (9.721279e-06, None, 0.50, 's'),
-                'time_2_1000000': (4.934530e-04, None, 0.50, 's'),
-                'time_4_10': (5.878997e-06, None, 0.50, 's'),
-                'time_4_10000': (1.495080e-05, None, 0.50, 's'),
-                'time_4_1000000': (6.791397e-04, None, 0.50, 's'),
-                'time_6_10': (5.428815e-06, None, 0.50, 's'),
-                'time_6_10000': (1.540580e-05, None, 0.50, 's'),
-                'time_6_1000000': (9.179296e-04, None, 0.50, 's')
-            },
-            'daint:gpu': {
-                'time_2_10': (3.925395e-06, None, 0.50, 's'),
-                'time_2_10000': (9.721279e-06, None, 0.50, 's'),
-                'time_2_1000000': (4.934530e-04, None, 0.50, 's'),
-                'time_4_10': (5.878997e-06, None, 0.50, 's'),
-                'time_4_10000': (1.495080e-05, None, 0.50, 's'),
-                'time_4_1000000': (6.791397e-04, None, 0.50, 's'),
-                'time_6_10': (5.428815e-06, None, 0.50, 's'),
-                'time_6_10000': (1.540580e-05, None, 0.50, 's'),
-                'time_6_1000000': (9.179296e-04, None, 0.50, 's')
-            },
-        }
+    @rfm.run_before('run')
+    def set_memory_limit(self):
+        self.job.options = ['--mem-per-cpu=4G']
 
-        self.maintainers = ['AJ']
-        self.tags = {'benchmark'}
-
-    @rfm.run_before('compile')
-    def pgi_workaround(self):
-        if self.current_system.name in ['daint', 'dom']:
-            if self.current_environ.name == 'PrgEnv-pgi':
-                self.variables = {
-                    'CUDA_HOME': '$CUDATOOLKIT_HOME',
-                }
