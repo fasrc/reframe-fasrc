1de93630388474c0e3d88b52ae8f2575036230c8
Paul Edmon <pedmon@cfa.harvard.edu>
2026-06-04 11:20:17 -0400

    Probably easiest just to go with previous formatting.

--- checks/microbenchmarks/gpu/gpu_fryer/gpu_fryer.py
+++ checks/microbenchmarks/gpu/gpu_fryer/gpu_fryer.py
@@ -10,45 +10,46 @@ import reframe.utility.sanity as sn
 
 @rfm.simple_test
 class GPUFryerFP32TensorTest(rfm.RegressionTest):
-  valid_systems = ['cannon:local-gpu','cannon:gpu_test','fasse:fasse_gpu','test:gpu','arm:local']
-  build_system = 'SingleSource'
-  executable = 'timeout -s 9 2m singularity run --nv --bind /usr/lib64/libnvidia-ml.so.1:/usr/lib/x86_64-linux-gnu/libnvidia-ml.so.1 gpu-fryer_1.1.0.sif --use-fp32 60'
-  valid_prog_environs = ['gpu']
-  time_limit = '10m'
-  perf_patterns = {
-    'perf': sn.min(sn.extractall(
-      r'^\s*\[[^\]]*\]\s*GPU\s*\d+: (?P<fp>\S+) Gflops/s',
-      stdout, 'fp', float))
-  }
-  reference = {
-    'cannon:local-gpu': {
-      'perf': (5.2, -0.1, None, 'Gflops/s per gpu')
-    },
-    'cannon:gpu_test': {
-      'perf': (5.2, -0.1, None, 'Gflops/s per gpu')
-    },
-    '*': {
-      'perf': (3.35, None, None, 'Gflops/s per gpu')
-    },
-  }
+    def __init__(self):
+        self.valid_systems = ['cannon:local-gpu','cannon:gpu_test','fasse:fasse_gpu','test:gpu','arm:local']
+        self.build_system = 'SingleSource'
+        self.executable = 'timeout -s 9 2m singularity run --nv --bind /usr/lib64/libnvidia-ml.so.1:/usr/lib/x86_64-linux-gnu/libnvidia-ml.so.1 gpu-fryer_1.1.0.sif --use-fp32 60'
+        self.valid_prog_environs = ['gpu']
+        self.time_limit = '10m'
+        self.perf_patterns = {
+            'perf': sn.min(sn.extractall(
+                r'^\s*\[[^\]]*\]\s*GPU\s*\d+: (?P<fp>\S+) Gflops/s',
+                self.stdout, 'fp', float))
+        }
+        self.reference = {
+            'cannon:local-gpu': {
+                'perf': (5.2, -0.1, None, 'Gflops/s per gpu')
+            },
+            'cannon:gpu_test': {
+                'perf': (5.2, -0.1, None, 'Gflops/s per gpu')
+            },
+            '*': {
+                'perf': (3.35, None, None, 'Gflops/s per gpu')
+            },
+        }
 
-  @sanity_function
-  def assert_sanity(self):
-    return sn.assert_found(r'All GPUs seem healthy', self.stdout)
+    @sanity_function
+    def assert_sanity(self):
+        return sn.assert_found(r'All GPUs seem healthy', self.stdout)
 
 
-  @run_before('run')
-  def set_gpus_per_node(self):
-    cp = self.current_partition.fullname
-    if cp in {'cannon:local-gpu', 'fasse:fasse_gpu', 'test:gpu'}:
-      self.num_gpus_per_node = 4
-      self.num_cpus_per_task = 4
-      self.num_tasks = 1
-    else:
-      self.num_gpus_per_node = 1
-      self.num_cpus_per_task = 1
-      self.num_tasks = 1
+    @run_before('run')
+    def set_gpus_per_node(self):
+        cp = self.current_partition.fullname
+        if cp in {'cannon:local-gpu', 'fasse:fasse_gpu', 'test:gpu'}:
+            self.num_gpus_per_node = 4
+            self.num_cpus_per_task = 4
+            self.num_tasks = 1
+        else:
+            self.num_gpus_per_node = 1
+            self.num_cpus_per_task = 1
+            self.num_tasks = 1
 
-  @run_before('run')
-  def set_memory_limit(self):
-    self.job.options = ['--mem-per-cpu=4G']
+    @run_before('run')
+    def set_memory_limit(self):
+        self.job.options = ['--mem-per-cpu=4G']
